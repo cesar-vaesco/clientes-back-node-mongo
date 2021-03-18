cd8625390b83bcc7386e8618f0b02d4db453a45d
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-18 17:09:50 -0600

    'put actualizarProducto - get listarProducto'

--- routes/producto.js
+++ routes/producto.js
@@ -5,8 +5,12 @@ const router = express.Router();
 const productoController = require('../controllers/ProductoController');
 
 //api/productos
+router.get('/', productoController.obtenerProductos);
+
+router.get('/:id', productoController.obtenerProducto);
+
 router.post('/', productoController.crearProducto);
 
-router.get('/', productoController.obtenerProductos);
+router.put('/:id', productoController.actualizarProducto);
 
 module.exports = router
