359c37dc1d6243d240841b242e073c65f2238aa2
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-18 02:06:10 -0600

    Primeros enlaces al servidor

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Desde index.js');
+const express= require('express');
+
+// Creamos servidor
+
+const app = express();
+
+//Definimos ruta principal
+
+app.get('/', (req, res) =>{
+    res.send('Hola mundo');
+})
+app.listen(4000, () =>{
+    console.log('Servidor en uso....')
+})
