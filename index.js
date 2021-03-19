46b332dfbae807844c7acba522a209361dbb3f29
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-18 18:04:59 -0600

    Configurando cors para comunicar back con front

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
 // Creamos servidor
 const app = express();
 
 //Conectamos a la base de datos
 conectarDB();
-
+app.use(cors());
 //Middleware: express traduce el dato que recibe y lo entrega en formato json
 app.use(express.json());
 
